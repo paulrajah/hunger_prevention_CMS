8ff5deb445bc252dffcbe4516fe04bf26abcf65a
paulrajah <pr257@njit.edu>
2014-10-27 11:14:18 -0400

    initial upload of files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HungerPrevention::Application.config.secret_key_base = '804990dc8d6bf16d101a0e3346a2df3de221239b727fcac84c462a4bbb8186d8e32e75eeee550f7a54d7a44b426b3b61bafddb52c933f66e813607f55942d7a8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HungerPrevention::Application.config.session_store :cookie_store, key: '_hungerPrevention_session'
